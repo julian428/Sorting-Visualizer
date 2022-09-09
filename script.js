a17cdb5863d9f48c0d02ff476b1fe4a1a7fb3fbf
Juluś <57361466+julian428@users.noreply.github.com>
2022-09-09 21:50:04 +0200

    Add files via upload
    added info sections to each sorting type

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ let randomizeId = 'randomize';
 let speedSelectorId = 'speed';
 let sizeSelectorId = 'size';
 let sortSelectorId = 'sortSelect';
+let infoNum = {'bubble': 0, 'selection': 1, 'insertion': 2, 'heap': 3, 'quick': 4};
 let getId = (id) => document.getElementById(id);
 let bars = parseInt(getId(sizeSelectorId).value);
 let speed = getId(speedSelectorId).value;
 let sortAlgo = getId(sortSelectorId).value;
+let oldSortAlgo = sortAlgo;
 let unsortedArray = new Array(bars);
 
 window.onload = () => {
+    render().info();
     getId(randomizeId).addEventListener('click', () => {
         unsortedArray = array().randomize();
         render().bars();
@@ -24,7 +27,11 @@ window.onload = () => {
         bars = parseInt(getId(sizeSelectorId).value);
         unsortedArray = new Array(bars)
     });
-    getId(sortSelectorId).addEventListener('input', () => sortAlgo = getId(sortSelectorId).value);
+    getId(sortSelectorId).addEventListener('input', () => {
+        oldSortAlgo = sortAlgo;
+        sortAlgo = getId(sortSelectorId).value;
+        render().info()
+    });
 }
 
 function sleep(ms) {
@@ -38,10 +45,17 @@ function render(){
             for(i = 0; i < unsortedArray.length; i++){
                 let bar = document.createElement('div');
                 bar.classList.add('bar');
-                bar.style.width = 90 / array.length + 'vw';
+                bar.style.width = 100 / array.length + 'vw';
                 bar.style.height = array[i] + "px";
                 getId(sortId).appendChild(bar);
             }
+        },
+        info: (algo = sortAlgo, oldAlgo = oldSortAlgo) => {
+            let info = document.getElementsByClassName('info');
+            let old = infoNum[oldAlgo];
+            let now = infoNum[algo];
+            info[old].style.display = 'none';
+            info[now].style.display = 'flex';
         }
     }
     return self;
@@ -51,7 +65,7 @@ function array(){
     const self = {
         randomize: (array = unsortedArray) => {
             for(i = 0; i < array.length; i++){
-                array[i] = Math.floor((Math.random()*500)+1)
+                array[i] = Math.floor((Math.random()*400)+1)
             }
             return array;
         }
@@ -72,6 +86,12 @@ function sort(){
                 case 'insertion':
                     self.insertion()
                     break;
+                case 'heap':
+                    window.alert('To jeszcze nie dziala')
+                    //self.heap()
+                    break;
+                case 'quick':
+                    window.alert('To jeszcze nie dziala')
                 default:
                     self.bubble();
                     break;
@@ -82,17 +102,20 @@ function sort(){
             for(let i = 0; i < array.length; i++){
                 for(let j = 0; j < (array.length - i - 1); j++)
                 {
+                    bars[j].style.backgroundColor = "lightgreen";
+                    bars[j + 1].style.backgroundColor = "red";
+                    await sleep(speed);
+                    render().bars();
                     if(array[j] > array[j+1]){
                         let temp = array[j];
                         array[j] = array[j + 1];
                         array[j+1] = temp;
-                        bars[j].style.backgroundColor = "lightgreen";
-                        bars[j + 1].style.backgroundColor = "red";
-                        await sleep(speed);
+                        bars[j+1].style.backgroundColor = "lightgreen";
+                        bars[j].style.backgroundColor = "red";
                         render().bars();
+                        await sleep(speed);
                     }
                 }
-                await sleep(speed);
                 render().bars();
             }
         },
@@ -142,6 +165,49 @@ function sort(){
                 }
                 array[j+1] = current;
             }
+        },
+        heap: (array = unsortedArray) => {
+            let parentIndex = (index) => Math.floor((index-1)/2);
+            let leftChildIndex = (index) => 2*index+1;
+            let rightChildIndex = (index) => 2*index+2;
+            let swap = (a, b) => {
+                let temp = array[a];
+                array[a] = array[b];
+                array[b] = temp;
+            }
+
+            let insert = (item) => {
+                array.push(item);
+                let index = array.length - 1;
+                let parent = parentIndex(index);
+                while(array[parent] && array[parent] < array[index]){
+                    swap(parent, index);
+                    index = parentIndex(index);
+                    parent = parentIndex(index);
+                }
+            }
+
+            let del = () => {
+                let item = array.shift();
+                array.unshift(array.pop());
+                let index = 0;
+                let leftChild = leftChildIndex(index);
+                let rightChild = rightChildIndex(index);
+                while(array[leftChild] && array[leftChild] > array[index] || array[rightChild] > array[index]){
+                    let max = leftChild;
+                    if(array[rightChild] && array[rightChild] > array[max]) max = rightChild;
+                    swap(max, index);
+                    index = max;
+                    leftChild = leftChildIndex(max);
+                    rightChild = rightChildIndex(max);
+                }
+                return item;
+            }
+
+            let sorted = [];
+            for(let i = 0; i < array.length; i++)   insert(array[i]);
+            for(let i = 0; i < array.length; i++)   sorted.push(del());
+            render().bars(sorted);
         }
     }
     return self;
