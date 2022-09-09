ae1e0360b7a50fec883c407acdf78d25fe612ba9
Juluś <57361466+julian428@users.noreply.github.com>
2022-09-09 15:40:21 +0200

    Add files via upload
    Added insertion and select sort

--- script.js
+++ script.js
@@ -10,6 +10,23 @@ let speed = getId(speedSelectorId).value;
 let sortAlgo = getId(sortSelectorId).value;
 let unsortedArray = new Array(bars);
 
+window.onload = () => {
+    getId(randomizeId).addEventListener('click', () => {
+        unsortedArray = array().randomize();
+        render().bars();
+    });
+    getId(sortButtonId).addEventListener('click', () => {
+        sort().start();
+        render().bars();
+    });
+    getId(speedSelectorId).addEventListener('input', () => speed = getId(speedSelectorId).value);
+    getId(sizeSelectorId).addEventListener('input', () => {
+        bars = parseInt(getId(sizeSelectorId).value);
+        unsortedArray = new Array(bars)
+    });
+    getId(sortSelectorId).addEventListener('input', () => sortAlgo = getId(sortSelectorId).value);
+}
+
 function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -23,7 +40,6 @@ function render(){
                 bar.classList.add('bar');
                 bar.style.width = 90 / array.length + 'vw';
                 bar.style.height = array[i] + "px";
-                bar.style.color = 'black'
                 getId(sortId).appendChild(bar);
             }
         }
@@ -50,6 +66,12 @@ function sort(){
                 case 'bubble':
                     self.bubble();
                     break;
+                case 'selection':
+                    self.selection();
+                    break;
+                case 'insertion':
+                    self.insertion()
+                    break;
                 default:
                     self.bubble();
                     break;
@@ -60,8 +82,6 @@ function sort(){
             for(let i = 0; i < array.length; i++){
                 for(let j = 0; j < (array.length - i - 1); j++)
                 {
-                    bars[j].style.backgroundColor = "lightgreen";
-                    bars[j + 1].style.backgroundColor = "red";
                     if(array[j] > array[j+1]){
                         let temp = array[j];
                         array[j] = array[j + 1];
@@ -71,31 +91,59 @@ function sort(){
                         await sleep(speed);
                         render().bars();
                     }
-                    bars[j].style.backgroundColor = "var(--comp)";
-                    bars[j + 1].style.backgroundColor = "var(--comp)";
                 }
                 await sleep(speed);
                 render().bars();
             }
-            return array;
+        },
+        selection: async (array = unsortedArray) => {
+            let bars = document.getElementsByClassName("bar");
+            for(let i = 0; i < array.length; i++){
+                let min = i;
+                bars[min].style.backgroundColor = 'red';
+                await sleep(speed);
+                render().bars()
+                for(let j = i+1; j < array.length; j++){
+                    if(array[j]<array[min]){
+                        min = j;
+                        bars[min].style.backgroundColor = 'red';
+                        render().bars()
+                    }
+                    bars[j].style.backgroundColor = 'lightgreen';
+                    bars[min].style.backgroundColor = 'red';
+                    await sleep(speed);
+                    render().bars();
+                }
+                if(min != i){
+                    let temp = array[i];
+                    array[i] = array[min];
+                    array[min] = temp;
+                    bars[min].style.backgroundColor = 'red';
+                }
+                render().bars();
+            }
+            render().bars();
+        },
+        insertion: async (array = unsortedArray) => {
+            let bars = document.getElementsByClassName("bar");
+            for(let i = 1; i < array.length; i++){
+                let current = array[i];
+                bars[i].style.backgroundColor = 'red';
+                await sleep(speed);
+                render().bars();
+                let j = i-1;
+                while((j > -1) && (current < array[j])){
+                    bars[i].style.backgroundColor = 'red';
+                    bars[j+1].style.backgroundColor = 'lightgreen';
+                    await sleep(speed);
+                    render().bars();
+                    array[j+1] = array[j];
+                    j--;
+                }
+                array[j+1] = current;
+            }
         }
     }
     return self;
 }
 
-
-window.onload = () => {
-    getId(randomizeId).addEventListener('click', () => {
-        unsortedArray = array().randomize();
-        render().bars();
-    });
-    getId(sortButtonId).addEventListener('click', () => {
-        sort().start();
-        render().bars();
-    });
-    getId(speedSelectorId).addEventListener('input', () => speed = getId(speedSelectorId).value);
-    getId(sizeSelectorId).addEventListener('input', () => {
-        bars = parseInt(getId(sizeSelectorId).value);
-        unsortedArray = new Array(bars)
-    });
-}
